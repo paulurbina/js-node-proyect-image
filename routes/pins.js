1d1c035543abaff6373b95ca255fc30010d220cb
calisTONY97 <paulurbian@hotmail.com>
2019-07-25 14:34:14 -0500

    save pins a loca and db

--- routes/pins.js
+++ routes/pins.js
@@ -1,6 +1,54 @@
+const path = require('path');
+// bring Pin model
+const Pin = require('../models/Pin');
+
 module.exports = (app) => {
     app.route('/pins/create')
         .get((req, res, next) => {
+            //render the model to create pin
             res.render('pins/create');
+        })
+        .post((req, res, next) => {
+            // create new model Pin
+            let pin = new Pin();
+            // elements Pin
+            pin.title = req.body.title;
+            pin.desc = req.body.desc;
+            pin.username = req.body.username;
+            pin.isSave = false;
+
+            if(!req.files) {
+                return json('error')
+            }
+
+            // check file
+            let sampleFile = req.files.sampleFile;
+            // add name to file
+            let fileName = Math.random().toString(30).slice(2) + '.jpg';
+            // check file location local
+            let path = './public/files/' + fileName;
+            // for the model
+            pin.path = '/files/' + fileName;
+
+            // move to local path
+            sampleFile.mv(path, (err) => {
+                if(err) {
+                    return res.status(500).send(err);
+                }
+            });
+
+            // save pin a db
+            pin.save((err) => {
+                if(err) throw err;
+                res.redirect('/pins/index');
+            });
+
+        });
+        // search all pins
+        app.get('/pins/index', (req, res, next) => {
+            Pin.find({}, (err, pins) => {
+                // send pins a index template
+                res.render('pins/index', { pins })
+            })
         });
 }
